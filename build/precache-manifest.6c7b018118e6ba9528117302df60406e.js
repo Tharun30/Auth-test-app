ff0a93924b9a0ab61f5bcbbf4fac20de716b5727
Abburi, Tharun <tharun_abburi@mcafee.com>
2025-09-05 20:36:21 +0530

    Deploy build files to gh-pages branch
    - Added all production build files to root directory
    - Updated .gitignore for gh-pages branch
    - Ready for GitHub Pages deployment at /auth-test-app

---
+++ build/precache-manifest.6c7b018118e6ba9528117302df60406e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f07e550cd218c022907cf479f77fee49",
+    "url": "/auth-test-app/index.html"
+  },
+  {
+    "revision": "0d5b3a257314f61a075d",
+    "url": "/auth-test-app/static/css/main.f30ea17d.chunk.css"
+  },
+  {
+    "revision": "0cdcf0842ab5ae8c85bf",
+    "url": "/auth-test-app/static/js/2.7ce1184d.chunk.js"
+  },
+  {
+    "revision": "923989a2bf141788204e01c0e9550d87",
+    "url": "/auth-test-app/static/js/2.7ce1184d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d5b3a257314f61a075d",
+    "url": "/auth-test-app/static/js/main.30386f83.chunk.js"
+  },
+  {
+    "revision": "aba8c0b66dfe03042928",
+    "url": "/auth-test-app/static/js/runtime-main.c56b88fe.js"
+  },
+  {
+    "revision": "467099806ce55916d7094b4c6fd0cdf4",
+    "url": "/auth-test-app/static/media/loading.46709980.svg"
+  }
+]);
